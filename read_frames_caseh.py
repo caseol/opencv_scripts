2428db2057b3eaf54f9dd2670670161f7dc5cd22
coliveira <coliveira@ansertecnologia.com>
2021-07-31 16:32:25 -0300

    Regra piscagem

--- read_frames_caseh.py
+++ read_frames_caseh.py
@@ -28,15 +28,15 @@ def control_led(retry_num, max_retry, led_current_status):
 	if int(retry_num) > 0 and int(retry_num) <= int(max_retry):
 		if int(retry_num) >= int(float(max_retry) * 0.85):
 			led_red.blink(0.25, 0.25)
-			led_green.off()
+			# led_green.on()
 			print("[RECON] PISCAR LED 0.25 - DateTime: " + dtn.strftime('%Y-%m-%d_%H_%M_%S'))
 		elif int(retry_num) >= int(float(max_retry) * 0.55):
 			led_red.blink(0.5, 0.5)
-			led_green.off()
+			# led_green.on()
 			print("[RECON] PISCAR LED 0.5 - DateTime: " + dtn.strftime('%Y-%m-%d_%H_%M_%S'))
 		else:
 			led_red.blink(0.75, 0.75)
-			led_green.on()
+			# led_green.on()
 			print("[RECON] PISCAR LED 0.75 - DateTime: " + dtn.strftime('%Y-%m-%d_%H_%M_%S'))
 	else:
 		# Se não estiver dentro das retentativas coloca o estado atual
